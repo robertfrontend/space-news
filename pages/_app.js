5646228219dbc611d9be59268c2b7d4b029f550b
Robert Romero Medina <enanow41romero@gmail.com>
2021-11-20 11:12:28 -0400

    initial commit

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import "../styles/globals.css";
+import "bootswatch/dist/flatly/bootstrap.min.css";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
